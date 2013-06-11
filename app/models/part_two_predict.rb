aa11314fe64981e09d4233acf56dc2a5c6094c44
Tsuyoshi Okumura <t-okumura@san-ei-med.co.jp>
2013-06-12 01:23:48 +0900

    みんなの予想（パート２）のデザインを変更

--- app/models/part_two_predict.rb
+++ app/models/part_two_predict.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class PartTwoPredict < ActiveRecord::Base
   attr_accessible :team_a, :team_b, :team_c, :team_d, :user_id
   belongs_to :user
@@ -11,5 +12,20 @@ class PartTwoPredict < ActiveRecord::Base
     pop.team_c = ""
     pop.team_d = ""
     pop.save
+  end
+
+  def self.get_your_predict_label(user_id, country)
+    user = User.find(user_id)
+    label = false
+    if user.part_two_predict.team_a == country
+      label = true 
+    elsif user.part_two_predict.team_b == country
+      label = true 
+    elsif user.part_two_predict.team_c == country
+      label = true 
+    elsif user.part_two_predict.team_d == country
+      label = true 
+    end
+    label
   end
 end
